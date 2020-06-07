e23ccdc25240d6b66af379f9e390215333d5b973
unknown <pneumoulramicro3.7@gmail.com>
2020-06-07 18:57:22 +0900

    deal with TimePicker and Calendar in TimeFragment

--- app/src/main/java/com/parallel/hati/randomalarm/TimeFragment.kt
+++ app/src/main/java/com/parallel/hati/randomalarm/TimeFragment.kt
@@ -1,13 +1,17 @@
 package com.parallel.hati.randomalarm
 
+import android.os.Build
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.TimePicker
+import androidx.annotation.RequiresApi
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
+import java.util.*
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
@@ -23,10 +27,38 @@ class TimeFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_time, container, false)
     }
 
+
+
+    @RequiresApi(Build.VERSION_CODES.M)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val timePicker = view.findViewById<TimePicker>(R.id.time_picker)
+
+        var hour = 8
+        var minute = 30
+
+        val currentApiVersion = Build.VERSION.SDK_INT
+        if (currentApiVersion >= Build.VERSION_CODES.M) {
+            timePicker.setHour(hour);
+            timePicker.setMinute(minute);
+        } else {
+            timePicker.setCurrentHour(hour);
+            timePicker.setCurrentMinute(minute);
+        }
+
         view.findViewById<Button>(R.id.button_second).setOnClickListener {
+            if (currentApiVersion >= Build.VERSION_CODES.M) {
+                hour = timePicker.getHour();
+                minute = timePicker.getMinute();
+
+            } else {
+                hour = timePicker.getCurrentHour();
+                minute = timePicker.getCurrentMinute();
+            }
+            val calendar : Calendar = Calendar.getInstance()
+            calendar.set(Calendar.HOUR_OF_DAY, hour)
+            calendar.set(Calendar.MINUTE, minute)
             findNavController().navigate(R.id.action_TimeFragment_to_MainFragment)
         }
     }
