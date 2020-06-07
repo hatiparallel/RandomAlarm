3752b1343a39b7735a092c24ac15334e3911804b
unknown <pneumoulramicro3.7@gmail.com>
2020-06-07 18:14:56 +0900

    add args to TimeFragment

--- app/src/main/java/com/parallel/hati/randomalarm/TimeFragment.kt
+++ app/src/main/java/com/parallel/hati/randomalarm/TimeFragment.kt
@@ -7,11 +7,13 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
  */
 class TimeFragment : Fragment() {
+    private val args: TimeFragmentArgs by navArgs()
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
