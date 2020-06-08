5d158885a9353b15d92453d93e22fa23816179fa
unknown <pneumoulramicro3.7@gmail.com>
2020-06-08 10:29:56 +0900

    call AlarmReceiver (detected with Toast, but not with Log)

--- app/src/main/java/com/parallel/hati/randomalarm/AlarmReceiver.kt
+++ app/src/main/java/com/parallel/hati/randomalarm/AlarmReceiver.kt
@@ -4,10 +4,12 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.util.Log
+import android.widget.Toast
 
 class AlarmReceiver() : BroadcastReceiver() {
     override fun onReceive(context : Context, intent : Intent) {
         Log.d("MMM", "received")
+        Toast.makeText(context,"Received",Toast.LENGTH_LONG).show()
         return
     }
 }
--- app/src/main/java/com/parallel/hati/randomalarm/TimeFragment.kt
+++ app/src/main/java/com/parallel/hati/randomalarm/TimeFragment.kt
@@ -1,5 +1,10 @@
 package com.parallel.hati.randomalarm
 
+import android.app.AlarmManager
+import android.app.AlarmManager.AlarmClockInfo
+import android.app.PendingIntent
+import android.content.Context.ALARM_SERVICE
+import android.content.Intent
 import android.os.Build
 import android.os.Bundle
 import android.util.Log
@@ -74,8 +79,39 @@ class TimeFragment : Fragment() {
             }
 
             val calendar : Calendar = Calendar.getInstance()
+            calendar.setTimeInMillis(System.currentTimeMillis())
+            Log.d("MMM", calendar.get(Calendar.DATE).toString())
+            Log.d("MMM", calendar.get(Calendar.MONTH).toString())
+            Log.d("MMM", calendar.get(Calendar.YEAR).toString())
+            Log.d("MMM", calendar.get(Calendar.HOUR_OF_DAY).toString())
+            Log.d("MMM", calendar.get(Calendar.MINUTE).toString())
             calendar.set(Calendar.HOUR_OF_DAY, hour)
             calendar.set(Calendar.MINUTE, minute)
+            calendar.set(Calendar.SECOND, 0)
+            if (calendar.getTimeInMillis() <= System.currentTimeMillis()) {
+                calendar.add(Calendar.DATE, 1)
+            }
+            Log.d("MMM", calendar.get(Calendar.DATE).toString())
+            Log.d("MMM", calendar.get(Calendar.MONTH).toString())
+            Log.d("MMM", calendar.get(Calendar.YEAR).toString())
+            Log.d("MMM", calendar.get(Calendar.HOUR_OF_DAY).toString())
+            Log.d("MMM", calendar.get(Calendar.MINUTE).toString())
+
+            val intent = Intent(this.getContext(), AlarmReceiver::class.java)
+            val pending = PendingIntent.getBroadcast(this.getContext(), 0, intent, 0)
+            val manager = this.getContext()!!.getSystemService(ALARM_SERVICE) as AlarmManager
+            Log.d("MMM", manager.toString())
+            // manager.setExact(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), pending)
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                manager.setAlarmClock(
+                    AlarmClockInfo(calendar.timeInMillis, null),
+                    pending
+                )
+            } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+                manager.setExact(AlarmManager.RTC_WAKEUP, calendar.timeInMillis, pending)
+            } else {
+                manager.set(AlarmManager.RTC_WAKEUP, calendar.timeInMillis, pending)
+            }
 
             val content = TimeFragmentDirections.actionTimeFragmentToMainFragment(hour, minute)
             findNavController().navigate(content)
